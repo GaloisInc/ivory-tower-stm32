eb0b0cbe7e89dd6f22a5f714099dd2c57ecb8558
Pat Hickey <pat@galois.com>
2014-11-10 19:13:23 -0800

    tower-freertos-stm32: tower-config Configurable instances

--- tower-freertos-stm32/src/Ivory/OS/FreeRTOS/Tower/STM32/Config.hs
+++ tower-freertos-stm32/src/Ivory/OS/FreeRTOS/Tower/STM32/Config.hs
@@ -8,6 +8,7 @@ module Ivory.OS.FreeRTOS.Tower.STM32.Config
 
 import Ivory.BSP.STM32.ClockConfig
 import Ivory.BSP.STM32.Processor
+import Tower.Config
 
 data Config =
   Config
@@ -19,4 +20,26 @@ data Config =
 data Bootloader
   = NoBootloader
   | PX4ProjectBootloader
+
+instance Configurable Bootloader where
+  fromConfig v = case fromConfig v of
+    Just False -> Just NoBootloader
+    _ -> case fromConfig v of
+      Just "none"                 -> Just NoBootloader
+      Just "NoBootloader"         -> Just NoBootloader
+      Just "px4"                  -> Just PX4ProjectBootloader
+      Just "PX4ProjectBootloader" -> Just PX4ProjectBootloader
+      _ -> Nothing
+
+instance Configurable Config where
+  fromConfig v = do
+    body <- element "stm32" v
+    p <- fromConfig =<< element "processor" body
+    b <- fromConfig =<< element "bootloader" body
+    c <- fromConfig body
+    return Config
+      { config_processor = p
+      , config_bootloader = b
+      , config_clock = c
+      }
 
