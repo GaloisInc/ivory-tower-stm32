804948476801203e6ebaf45aecaea133d0a0ffdc
Pat Hickey <pat@galois.com>
2014-11-18 16:30:32 -0800

    ivory-bsp-tests: uart test works
    i'm 3 for 3 on these ported drivers working the first time i ran them
    on real hardware, not gonna lie that feels pretty good

--- ivory-bsp-tests/BSP/Tests/UART/TestApp.hs
+++ ivory-bsp-tests/BSP/Tests/UART/TestApp.hs
@@ -16,61 +16,66 @@ import BSP.Tests.Platforms
 import BSP.Tests.LED.Blink
 
 import Ivory.BSP.STM32.Driver.UART
-import Ivory.BSP.STM32.PlatformClock
+import Ivory.BSP.STM32.ClockConfig
+import qualified Ivory.BSP.STM32F405.Interrupt   as F405
 
 --------------------------------------------------------------------------------
 
-app :: forall p
-     . (ColoredLEDs p, PlatformClock p, BoardInitializer p, TestUART p)
-    => Tower p ()
-app = do
-  boardInitializer
+app :: (e -> ColoredLEDs)
+    -> (e -> ClockConfig)
+    -> (e -> TestUART F405.Interrupt)
+    -> Tower e ()
+app toleds tocc touart = do
+  e <- getEnv
   -- Starts two tasks: a blink task and a controller task.  Periodically blink
   -- the blue LED.
-  blink period [blueLED platform]
+  blink p [blueLED (toleds e)]
   -- A new queue
   redledctl <- channel
   -- Starts a UART (serial) task
-  (istream, ostream) <- uartTower (testUART platform) 115200 (Proxy :: Proxy 256)
+  (istream, ostream) <- uartTower tocc (testUART (touart e)) 115200 (Proxy :: Proxy 256)
   -- Start the task defined below
-  echoPrompt "hello world" ostream istream (src redledctl)
+  echoPrompt "hello world" ostream istream (fst redledctl)
   -- A task that takes control input (Boolean) from the echo prompt and controls
   -- the red LED based on it.
-  task "settableLED" $ ledController [redLED platform] (snk redledctl)
+  monitor "settableLED" $ ledController [redLED (toleds e)] (snd redledctl)
   where
-  platform = Proxy :: Proxy p
-  period = 333
+  p = Milliseconds 333
 
 --------------------------------------------------------------------------------
 
 echoPrompt :: String
-           -> ChannelSource (Stored Uint8)
-           -> ChannelSink   (Stored Uint8)
-           -> ChannelSource (Stored IBool)
+           -> ChanInput  (Stored Uint8)
+           -> ChanOutput (Stored Uint8)
+           -> ChanInput  (Stored IBool)
            -> Tower p ()
-echoPrompt greeting ostream istream ledctlstream = task "echoprompt" $ do
-  o      <- withChannelEmitter ostream      "ostream"
-  ledctl <- withChannelEmitter ledctlstream "ledctl"
+echoPrompt greeting ostream istream ledctl = monitor "echoprompt" $ do
 
-  let puts :: (GetAlloc eff ~ Scope cs) => String -> Ivory eff ()
-      puts str = mapM_ (\c -> putc (fromIntegral (ord c))) str
+  let puts :: (GetAlloc eff ~ Scope cs)
+           => Emitter (Stored Uint8) -> String -> Ivory eff ()
+      puts e str = mapM_ (\c -> putc e (fromIntegral (ord c))) str
 
-      putc :: (GetAlloc eff ~ Scope cs) => Uint8 -> Ivory eff ()
-      putc = emitV_ o
+      putc :: (GetAlloc eff ~ Scope cs)
+           => Emitter (Stored Uint8) -> Uint8 -> Ivory eff ()
+      putc = emitV
 
-  taskInit $ do
-    puts (greeting ++ "\n")
-    puts prompt
-  ievt <- withChannelEvent istream "istream"
-  handle ievt "istream" $ \inputref -> do
-    input <- deref inputref
-    putc input -- echo to terminal
-    let testChar = (input `isChar`)
-    cond_
-      [ testChar '1'  ==> emitV_ ledctl true
-      , testChar '2'  ==> emitV_ ledctl false
-      , testChar '\n' ==> puts prompt
-      ]
+  handler systemInit "init" $ do
+    o <- emitter ostream 32
+    callback $ const $ do
+      puts o (greeting ++ "\n")
+      puts o prompt
+
+  handler istream "istream" $ do
+    l <- emitter ledctl 1
+    o <- emitter ostream 32
+    callbackV $ \input -> do
+      putc o input -- echo to terminal
+      let testChar = (input `isChar`)
+      cond_
+        [ testChar '1'  ==> emitV l true
+        , testChar '2'  ==> emitV l false
+        , testChar '\n' ==> puts o prompt
+        ]
   where prompt = "tower> "
 
 isChar :: Uint8 -> Char -> IBool
