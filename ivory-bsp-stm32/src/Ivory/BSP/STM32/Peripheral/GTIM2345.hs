9eace48d863f48bc97b0f6e146acc54651c322ab
Pat Hickey <pat@galois.com>
2015-02-16 11:15:37 -0800

    ivory-bsp-stm32: add missing umbrella modules for timer peripherals

---
+++ ivory-bsp-stm32/src/Ivory/BSP/STM32/Peripheral/GTIM2345.hs
@@ -0,0 +1,10 @@
+
+module Ivory.BSP.STM32.Peripheral.GTIM2345
+  ( module Ivory.BSP.STM32.Peripheral.GTIM2345.Peripheral
+  , module Ivory.BSP.STM32.Peripheral.GTIM2345.Regs
+  , module Ivory.BSP.STM32.Peripheral.GTIM2345.RegTypes
+  ) where
+
+import Ivory.BSP.STM32.Peripheral.GTIM2345.Peripheral
+import Ivory.BSP.STM32.Peripheral.GTIM2345.Regs
+import Ivory.BSP.STM32.Peripheral.GTIM2345.RegTypes
