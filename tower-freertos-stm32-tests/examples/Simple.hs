6ea87d5d30a5e6ed9509c9e9164253dd613234a7
Jamey Sharp <jamey@galois.com>
2015-03-18 09:45:57 -0700

    Fix STM32 backend tests for stm32ConfigParser change.

--- tower-freertos-stm32-tests/examples/Simple.hs
+++ tower-freertos-stm32-tests/examples/Simple.hs
@@ -35,5 +35,5 @@ test1_per _tocc = do
 main :: IO ()
 main = towerCompile p (test1_per stm32config_clock)
   where p topts = do
-            cfg <- getConfig topts stm32ConfigParser
+            cfg <- getConfig topts $ stm32ConfigParser $ stm32f405Defaults 24
             return $ stm32FreeRTOS id cfg
