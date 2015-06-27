c224be6df5d7c50d71bcc16c56ca9c1ea4ee3e0d
Pat Hickey <pat@galois.com>
2015-06-27 12:53:20 -0700

    ivory-bsp-tests: remove Debugger infastructure from dmaUART
    wasn't using it

--- ivory-bsp-stm32/src/Ivory/BSP/STM32/Driver/UART/DMA.hs
+++ ivory-bsp-stm32/src/Ivory/BSP/STM32/Driver/UART/DMA.hs
@@ -6,8 +6,6 @@
 
 module Ivory.BSP.STM32.Driver.UART.DMA
   ( dmaUARTTower
-  , dmaUARTTowerDebuggable
-  , DMAUARTTowerDebugger(..)
   ) where
 
 import Ivory.Language
@@ -27,28 +25,6 @@ import Ivory.BSP.STM32.Peripheral.DMA
 
 import Ivory.BSP.STM32.Driver.DMA
 
-data DMAUARTTowerDebugger =
-  DMAUARTTowerDebugger
-    { debug_init             :: forall eff . Ivory eff ()
-    , debug_isr              :: forall eff . Ivory eff ()
-    , debug_evthandler_start :: forall eff . Ivory eff ()
-    , debug_evthandler_end   :: forall eff . Ivory eff ()
-    , debug_txcheck          :: forall eff . Ivory eff ()
-    , debug_txcheck_pend     :: forall eff . Ivory eff ()
-    , debug_txeie            :: forall eff . IBool -> Ivory eff ()
-    }
-
-emptyDbg :: DMAUARTTowerDebugger
-emptyDbg =
-  DMAUARTTowerDebugger
-    { debug_init = return ()
-    , debug_isr  = return ()
-    , debug_evthandler_start = return ()
-    , debug_evthandler_end = return ()
-    , debug_txcheck = return ()
-    , debug_txcheck_pend = return ()
-    , debug_txeie = const (return ())
-    }
 
 dmaUARTTower :: (IvoryString tx, IvoryString rx)
              => (e -> ClockConfig)
@@ -58,18 +34,7 @@ dmaUARTTower :: (IvoryString tx, IvoryString rx)
              -> Integer
              -> Tower e ( BackpressureTransmit tx (Stored IBool)
                         , ChanOutput rx)
-dmaUARTTower tocc u p s b = dmaUARTTowerDebuggable tocc u p s b emptyDbg
-
-dmaUARTTowerDebuggable :: (IvoryString tx, IvoryString rx)
-                       => (e -> ClockConfig)
-                       -> DMAUART
-                       -> UARTPins
-                       -> DMATowerStreams
-                       -> Integer
-                       -> DMAUARTTowerDebugger
-                       -> Tower e ( BackpressureTransmit tx (Stored IBool)
-                                  , ChanOutput rx)
-dmaUARTTowerDebuggable tocc dmauart pins streams baud dbg = do
+dmaUARTTower tocc dmauart pins streams baud = do
   req_chan  <- channel
   resp_chan <- channel
   rx_chan   <- channel
@@ -78,7 +43,7 @@ dmaUARTTowerDebuggable tocc dmauart pins streams baud dbg = do
 
   monitor (uartName uart ++ "_dma_driver") $ do
     dmaUARTTowerMonitor tocc dmauart pins streams baud
-                        (fst rx_chan) (snd req_chan) (fst resp_chan) dbg
+                        (fst rx_chan) (snd req_chan) (fst resp_chan)
   return (BackpressureTransmit (fst req_chan) (snd resp_chan), (snd rx_chan))
   where
   uart = dmaUARTPeriph dmauart
@@ -92,9 +57,8 @@ dmaUARTTowerMonitor :: (IvoryString tx, IvoryString rx)
                     -> ChanInput rx
                     -> ChanOutput tx
                     -> ChanInput (Stored IBool)
-                    -> DMAUARTTowerDebugger
                     -> Monitor e ()
-dmaUARTTowerMonitor tocc dmauart pins streams baud rx_out_chan req_chan resp_chan dbg = do
+dmaUARTTowerMonitor tocc dmauart pins streams baud rx_out_chan req_chan resp_chan = do
   clockConfig <- fmap tocc getEnv
 
   monitorModuleDef $ do
@@ -107,7 +71,6 @@ dmaUARTTowerMonitor tocc dmauart pins streams baud rx_out_chan req_chan resp_cha
   rx1_buf <- state (named "rx1_buf")
 
   handler (dma_stream_init rxstream) "init" $ callback $ const $ do
-    debug_init dbg
     uartInit uart pins clockConfig (fromIntegral baud) False
     -- Enable TX interrupt:
     dma_stream_enable_int txstream
