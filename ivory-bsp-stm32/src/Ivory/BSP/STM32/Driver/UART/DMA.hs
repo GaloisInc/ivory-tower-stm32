a02ab8a1767431fb9ed472665ef986ebb88b46ab
Pat Hickey <pat@galois.com>
2015-06-27 15:28:27 -0700

    ivory-bsp-stm32: refactor uart dma driver to make code more managable
    functionality should be unchanged

--- ivory-bsp-stm32/src/Ivory/BSP/STM32/Driver/UART/DMA.hs
+++ ivory-bsp-stm32/src/Ivory/BSP/STM32/Driver/UART/DMA.hs
@@ -39,30 +39,53 @@ dmaUARTTower tocc dmauart pins streams baud rx_flush_per = do
   req_chan  <- channel
   resp_chan <- channel
   rx_chan   <- channel
+
+  dmauart_initialized <- channel
   p <- period rx_flush_per
 
   mapM_ towerArtifact dmaArtifacts
 
   monitor (uartName uart ++ "_dma_driver") $ do
-    dmaUARTTowerMonitor tocc dmauart pins streams baud
-                        (fst rx_chan) p (snd req_chan) (fst resp_chan)
+    dmaUARTHardwareMonitor tocc dmauart pins baud
+      dma_initialized (fst dmauart_initialized)
+
+    dmaUARTTransmitMonitor dmauart streams (snd req_chan) (fst resp_chan)
+      (snd dmauart_initialized)
+
+    dmaUARTReceiveMonitor  dmauart streams (fst rx_chan) p
+      (snd dmauart_initialized)
+
   return (BackpressureTransmit (fst req_chan) (snd resp_chan), (snd rx_chan))
+
+  where
+  uart = dmaUARTPeriph dmauart
+  dma_initialized = dma_stream_init $ dmaUARTRxStream  dmauart streams
+
+dmaUARTHardwareMonitor :: (e -> ClockConfig)
+                       -> DMAUART
+                       -> UARTPins
+                       -> Integer
+                       -> ChanOutput (Stored ITime)
+                       -> ChanInput (Stored ITime)
+                       -> Monitor e ()
+dmaUARTHardwareMonitor tocc dmauart pins baud init_chan init_cb = do
+  clockConfig <- fmap tocc getEnv
+  handler init_chan "dmauart_hw_init" $ do
+    e <- emitter init_cb 1
+    callback $ \t -> do
+      uartInit uart pins clockConfig (fromIntegral baud) False
+      emit e t
   where
   uart = dmaUARTPeriph dmauart
 
-dmaUARTTowerMonitor :: (IvoryString tx, IvoryString rx)
-                    => (e -> ClockConfig)
-                    -> DMAUART
-                    -> UARTPins
-                    -> DMATowerStreams
-                    -> Integer
-                    -> ChanInput rx
-                    -> ChanOutput (Stored ITime)
-                    -> ChanOutput tx
-                    -> ChanInput (Stored IBool)
-                    -> Monitor e ()
-dmaUARTTowerMonitor tocc dmauart pins streams baud rx_out_chan rx_flush_chan req_chan resp_chan = do
-  clockConfig <- fmap tocc getEnv
+dmaUARTTransmitMonitor :: (IvoryString tx)
+                       => DMAUART
+                       -> DMATowerStreams
+                       -> ChanOutput tx
+                       -> ChanInput  (Stored IBool)
+                       -> ChanOutput (Stored ITime)
+                       -> Monitor e ()
+dmaUARTTransmitMonitor dmauart streams req_chan resp_chan init_chan = do
 
   monitorModuleDef $ do
     hw_moduledef
@@ -70,131 +93,9 @@ dmaUARTTowerMonitor tocc dmauart pins streams baud rx_out_chan rx_flush_chan req
 
   req_buf <- state (named "req_buf")
 
-  rx0_buf <- state (named "rx0_buf")
-  rx1_buf <- state (named "rx1_buf")
-
-  handler (dma_stream_init rxstream) "init" $ callback $ const $ do
-    uartInit uart pins clockConfig (fromIntegral baud) False
+  handler init_chan "dmauart_tx_init" $ callback $ const $ do
     -- Enable TX interrupt:
     dma_stream_enable_int txstream
-
-    --------------------
-    -- Setup Recieve:
-
-    -- Set peripheral address:
-    setReg (dmaStreamPAR rx_regs) $
-      setField dma_sxpar_par (fromRep (bdr_reg_addr (uartRegDR uart)))
-
-    -- Set memory address:
-    buf0_start_addr <- call ref_to_uint32_proc ((rx0_buf ~> stringDataL) ! 0)
-    setReg (dmaStreamM0AR  rx_regs) $
-      setField dma_sxm0ar_m0a (fromRep buf0_start_addr)
-    buf1_start_addr <- call ref_to_uint32_proc ((rx1_buf ~> stringDataL) ! 0)
-    setReg (dmaStreamM1AR  rx_regs) $
-      setField dma_sxm1ar_m1a (fromRep buf1_start_addr)
-
-    -- Set number of data items to be transfered:
-    req_items <- assign (arrayLen (rx0_buf ~> stringDataL) - 1)
-    setReg (dmaStreamNDTR rx_regs) $
-      setField dma_sxndtr_ndt (fromRep req_items)
-
-    -- Set FIFO control register:
-    modifyReg (dmaStreamFCR rx_regs) $ do
-      setBit   dma_sxfcr_dmdis
-      setField dma_sxfcr_fth (fromRep 3)
-
-    -- Set control register:
-    modifyReg (dmaStreamCR rx_regs) $ do
-      setField dma_sxcr_chsel  (fromRep (fromIntegral rx_chan))
-      setField dma_sxcr_mburst (fromRep 0) -- Single (no burst)
-      setField dma_sxcr_pburst (fromRep 0) -- Single (no burst)
-      setField dma_sxcr_ct     (fromRep 0) -- Current Target buf 0
-      setField dma_sxcr_dbm    (fromRep 1) -- Double Buffering
-      setField dma_sxcr_pl     (fromRep 0) -- Priority 0 (highest)
-      setField dma_sxcr_msize  (fromRep 0) -- Byte memory size
-      setField dma_sxcr_psize  (fromRep 0) -- Byte peripheral size
-      setField dma_sxcr_minc   (fromRep 1) -- Increment according to msize
-      setField dma_sxcr_pinc   (fromRep 0) -- Fixed (no increment)
-      setField dma_sxcr_circ   (fromRep 0) -- No circular mode
-      setField dma_sxcr_dir    (fromRep 0) -- Peripheral to Memory
-      setField dma_sxcr_pfctrl (fromRep 0) -- DMA is flow controller
-      setField dma_sxcr_tcie   (fromRep 1) -- Enable transfer complete interrupt
-      setField dma_sxcr_htie   (fromRep 0) -- Disable half transfer interrupt
-      setField dma_sxcr_teie   (fromRep 1) -- Enable transfer error interrupt
-      setField dma_sxcr_dmeie  (fromRep 1) -- Enable direct mode error interrupt
-    -- Enable DMA Recieve in UART:
-    modifyReg (uartRegCR3 uart) $ do
-      setField uart_cr3_dmar (fromRep 1)
-
-    -- Set control register:
-    modifyReg (dmaStreamCR rx_regs) $ do
-      setField dma_sxcr_en  (fromRep 1) -- Enable Stream
-
-    -- Enable RX interrupt:
-    dma_stream_enable_int rxstream
-
-  -- Debugging states:
-  rx_complete     <- state (named "rx_complete")
-  rx_transfer_err <- state (named "rx_transfer_err")
-  rx_direct_err   <- state (named "rx_direct_err")
-
-  handler (dma_stream_signal rxstream) "rx_stream_interrupt" $ do
-    e <- emitter rx_out_chan 1
-    callback $ const $ do
-      -- Check stream flags:
-      flags <- dma_stream_get_isrflags rxstream
-
-      let incr r = r %= (+ (1 :: Uint32))
-      when (bitToBool (flags #. dma_isrflag_TCIF)) $ do
-        incr rx_complete
-      when (bitToBool (flags #. dma_isrflag_TEIF)) $ do
-        incr rx_transfer_err
-      when (bitToBool (flags #. dma_isrflag_DMEIF)) $ do
-        incr rx_direct_err
-
-      -- Clear stream flags:
-      dma_stream_clear_isrflags rxstream
-
-      -- XXX placeholder just to infer type of rx0 and rx1 bufs:
-      ifte_ false
-        (emit e (constRef rx0_buf))
-        (emit e (constRef rx1_buf))
-
-      -- Enable RX interrupt:
-      dma_stream_enable_int rxstream
-
-  handler rx_flush_chan "rx_flush" $ do
-    e <- emitter rx_out_chan 1
-    callback $ const $ do
-      -- Disable receive stream:
-      disableStream rx_regs
-      flags_rx <- dma_stream_get_isrflags rxstream
-      -- Get ndtr, reset to full size:
-      ndt_rx <- getReg (dmaStreamNDTR rx_regs)
-
-      req_items <- assign (arrayLen (rx0_buf ~> stringDataL) - 1)
-      setReg (dmaStreamNDTR rx_regs) $
-        setField dma_sxndtr_ndt (fromRep req_items)
-
-      -- Get current cr so we can switch ct (current target)
-      cr <- getReg (dmaStreamCR rx_regs)
-
-      -- start stream over again by clearing flags:
-      dma_stream_clear_isrflags rxstream
-      -- Switch current target, re-enable stream:
-      modifyReg (dmaStreamCR rx_regs) $ do
-        setField dma_sxcr_ct (boolToBit (iNot (bitToBool (cr #. dma_sxcr_ct))))
-        setField dma_sxcr_en (fromRep 1)
-
-      -- XXX just for inspecting that ping pong works properly from gdb:
-      ifte_ (bitToBool (cr #. dma_sxcr_ct))
-            (store (rx0_buf ~> stringLengthL) 1 >> store (rx1_buf ~> stringLengthL) 0)
-            (store (rx0_buf ~> stringLengthL) 0 >> store (rx1_buf ~> stringLengthL) 1)
-
-      -- XXX placeholder just to infer type of rx0 and rx1 bufs:
-      ifte_ false
-        (emit e (constRef rx0_buf))
-        (emit e (constRef rx1_buf))
 
 
   handler req_chan "req_chan" $ callback $ \req -> do
@@ -299,9 +200,7 @@ dmaUARTTowerMonitor tocc dmauart pins streams baud rx_out_chan rx_flush_chan req
       dma_stream_enable_int txstream
 
   where
-  rxstream = dmaUARTRxStream  dmauart streams
-  rx_chan  = dmaUARTRxChannel dmauart
-  rx_regs  = dma_stream_regs  rxstream
+
   txstream = dmaUARTTxStream  dmauart streams
   tx_chan  = dmaUARTTxChannel dmauart
   tx_regs  = dma_stream_regs  txstream
@@ -310,12 +209,153 @@ dmaUARTTowerMonitor tocc dmauart pins streams baud rx_out_chan rx_flush_chan req
   uart = dmaUARTPeriph dmauart
   named n = uartName uart ++ "_dma_" ++ n
 
-  ref_to_uint32_proc :: Def('[Ref s (Stored Uint8)] :-> Uint32)
-  ref_to_uint32_proc = importProc "ref_to_uint32" dmaRefToUint32Header
 
 
-  bdr_reg_addr :: BitDataReg a -> Uint32
-  bdr_reg_addr = fromInteger . unReg . bdr_reg
-    where unReg (Reg a) = a
+dmaUARTReceiveMonitor :: (IvoryString rx)
+                       => DMAUART
+                       -> DMATowerStreams
+                       -> ChanInput  rx
+                       -> ChanOutput (Stored ITime)
+                       -> ChanOutput (Stored ITime)
+                       -> Monitor e ()
+dmaUARTReceiveMonitor dmauart streams out_chan flush_chan init_chan = do
+  rx0_buf <- state (named "rx0_buf")
+  rx1_buf <- state (named "rx1_buf")
+
+  handler init_chan "dmauart_rx_init" $ callback $ const $ do
+    --------------------
+    -- Setup Recieve:
+
+    -- Set peripheral address:
+    setReg (dmaStreamPAR rx_regs) $
+      setField dma_sxpar_par (fromRep (bdr_reg_addr (uartRegDR uart)))
+
+    -- Set memory address:
+    buf0_start_addr <- call ref_to_uint32_proc ((rx0_buf ~> stringDataL) ! 0)
+    setReg (dmaStreamM0AR  rx_regs) $
+      setField dma_sxm0ar_m0a (fromRep buf0_start_addr)
+    buf1_start_addr <- call ref_to_uint32_proc ((rx1_buf ~> stringDataL) ! 0)
+    setReg (dmaStreamM1AR  rx_regs) $
+      setField dma_sxm1ar_m1a (fromRep buf1_start_addr)
+
+    -- Set number of data items to be transfered:
+    req_items <- assign (arrayLen (rx0_buf ~> stringDataL) - 1)
+    setReg (dmaStreamNDTR rx_regs) $
+      setField dma_sxndtr_ndt (fromRep req_items)
+
+    -- Set FIFO control register:
+    modifyReg (dmaStreamFCR rx_regs) $ do
+      setBit   dma_sxfcr_dmdis
+      setField dma_sxfcr_fth (fromRep 3)
+
+    -- Set control register:
+    modifyReg (dmaStreamCR rx_regs) $ do
+      setField dma_sxcr_chsel  (fromRep (fromIntegral rx_chan))
+      setField dma_sxcr_mburst (fromRep 0) -- Single (no burst)
+      setField dma_sxcr_pburst (fromRep 0) -- Single (no burst)
+      setField dma_sxcr_ct     (fromRep 0) -- Current Target buf 0
+      setField dma_sxcr_dbm    (fromRep 1) -- Double Buffering
+      setField dma_sxcr_pl     (fromRep 0) -- Priority 0 (highest)
+      setField dma_sxcr_msize  (fromRep 0) -- Byte memory size
+      setField dma_sxcr_psize  (fromRep 0) -- Byte peripheral size
+      setField dma_sxcr_minc   (fromRep 1) -- Increment according to msize
+      setField dma_sxcr_pinc   (fromRep 0) -- Fixed (no increment)
+      setField dma_sxcr_circ   (fromRep 0) -- No circular mode
+      setField dma_sxcr_dir    (fromRep 0) -- Peripheral to Memory
+      setField dma_sxcr_pfctrl (fromRep 0) -- DMA is flow controller
+      setField dma_sxcr_tcie   (fromRep 1) -- Enable transfer complete interrupt
+      setField dma_sxcr_htie   (fromRep 0) -- Disable half transfer interrupt
+      setField dma_sxcr_teie   (fromRep 1) -- Enable transfer error interrupt
+      setField dma_sxcr_dmeie  (fromRep 1) -- Enable direct mode error interrupt
+    -- Enable DMA Recieve in UART:
+    modifyReg (uartRegCR3 uart) $ do
+      setField uart_cr3_dmar (fromRep 1)
+
+    -- Set control register:
+    modifyReg (dmaStreamCR rx_regs) $ do
+      setField dma_sxcr_en  (fromRep 1) -- Enable Stream
+
+    -- Enable RX interrupt:
+    dma_stream_enable_int rxstream
+
+  -- Debugging states:
+  rx_complete     <- state (named "rx_complete")
+  rx_transfer_err <- state (named "rx_transfer_err")
+  rx_direct_err   <- state (named "rx_direct_err")
+
+  handler (dma_stream_signal rxstream) "rx_stream_interrupt" $ do
+    e <- emitter out_chan 1
+    callback $ const $ do
+      -- Check stream flags:
+      flags <- dma_stream_get_isrflags rxstream
+
+      let incr r = r %= (+ (1 :: Uint32))
+      when (bitToBool (flags #. dma_isrflag_TCIF)) $ do
+        incr rx_complete
+      when (bitToBool (flags #. dma_isrflag_TEIF)) $ do
+        incr rx_transfer_err
+      when (bitToBool (flags #. dma_isrflag_DMEIF)) $ do
+        incr rx_direct_err
+
+      -- Clear stream flags:
+      dma_stream_clear_isrflags rxstream
+
+      -- XXX placeholder just to infer type of rx0 and rx1 bufs:
+      ifte_ false
+        (emit e (constRef rx0_buf))
+        (emit e (constRef rx1_buf))
+
+      -- Enable RX interrupt:
+      dma_stream_enable_int rxstream
+
+  handler flush_chan "rx_flush" $ do
+    e <- emitter out_chan 1
+    callback $ const $ do
+      -- Disable receive stream:
+      disableStream rx_regs
+      flags_rx <- dma_stream_get_isrflags rxstream
+      -- Get ndtr, reset to full size:
+      ndt_rx <- getReg (dmaStreamNDTR rx_regs)
+
+      req_items <- assign (arrayLen (rx0_buf ~> stringDataL) - 1)
+      setReg (dmaStreamNDTR rx_regs) $
+        setField dma_sxndtr_ndt (fromRep req_items)
+
+      -- Get current cr so we can switch ct (current target)
+      cr <- getReg (dmaStreamCR rx_regs)
+
+      -- start stream over again by clearing flags:
+      dma_stream_clear_isrflags rxstream
+      -- Switch current target, re-enable stream:
+      modifyReg (dmaStreamCR rx_regs) $ do
+        setField dma_sxcr_ct (boolToBit (iNot (bitToBool (cr #. dma_sxcr_ct))))
+        setField dma_sxcr_en (fromRep 1)
+
+      -- XXX just for inspecting that ping pong works properly from gdb:
+      ifte_ (bitToBool (cr #. dma_sxcr_ct))
+            (store (rx0_buf ~> stringLengthL) 1 >> store (rx1_buf ~> stringLengthL) 0)
+            (store (rx0_buf ~> stringLengthL) 0 >> store (rx1_buf ~> stringLengthL) 1)
+
+      -- XXX placeholder just to infer type of rx0 and rx1 bufs:
+      ifte_ false
+        (emit e (constRef rx0_buf))
+        (emit e (constRef rx1_buf))
+
+
+
+  where
+  uart = dmaUARTPeriph dmauart
+  named n = uartName uart ++ "_dma_" ++ n
+
+  rxstream = dmaUARTRxStream  dmauart streams
+  rx_chan  = dmaUARTRxChannel dmauart
+  rx_regs  = dma_stream_regs  rxstream
+
+bdr_reg_addr :: BitDataReg a -> Uint32
+bdr_reg_addr = fromInteger . unReg . bdr_reg
+  where unReg (Reg a) = a
+
+ref_to_uint32_proc :: Def('[Ref s (Stored Uint8)] :-> Uint32)
+ref_to_uint32_proc = importProc "ref_to_uint32" dmaRefToUint32Header
 
 
