e4fae81afaae7d3f0dd6fc15fd5044c4568ef11e
Pat Hickey <pat@galois.com>
2015-06-29 13:15:06 -0700

    ivory-bsp-stm32: refactor DMA Driver to just instantiate a single stream

--- ivory-bsp-stm32/src/Ivory/BSP/STM32/Driver/DMA.hs
+++ ivory-bsp-stm32/src/Ivory/BSP/STM32/Driver/DMA.hs
@@ -7,9 +7,8 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module Ivory.BSP.STM32.Driver.DMA
-  ( DMATowerStreams(..)
-  , DMATowerStream(..)
-  , dmaTower
+  ( DMATowerStream(..)
+  , dmaTowerStream
   ) where
 
 import Ivory.Language
@@ -18,72 +17,33 @@ import Ivory.Tower
 import Ivory.BSP.STM32.Interrupt
 import Ivory.BSP.STM32.Peripheral.DMA
 
-data DMATowerStreams =
-  DMATowerStreams
-    { dma_stream_periph :: String
-    , dma_stream_0 :: DMATowerStream
-    , dma_stream_1 :: DMATowerStream
-    , dma_stream_2 :: DMATowerStream
-    , dma_stream_3 :: DMATowerStream
-    , dma_stream_4 :: DMATowerStream
-    , dma_stream_5 :: DMATowerStream
-    , dma_stream_6 :: DMATowerStream
-    , dma_stream_7 :: DMATowerStream
-    }
 
 data DMATowerStream =
   DMATowerStream
-    { dma_stream_init           :: ChanOutput (Stored ITime)
-    , dma_stream_signal         :: ChanOutput (Stored ITime)
+    { dma_stream_signal         :: ChanOutput (Stored ITime)
+    , dma_stream_channel        :: DMAChannel
     , dma_stream_regs           :: DMAStreamRegs
     , dma_stream_enable_int     :: forall eff . Ivory eff ()
     , dma_stream_get_isrflags   :: forall eff . Ivory eff DMA_ISRFlags
     , dma_stream_clear_isrflags :: forall eff . Ivory eff ()
     }
 
-dmaTower :: DMA -> Tower e DMATowerStreams
-dmaTower periph = do
+dmaTowerStream :: DMA -> DMAStream -> DMAChannel ->  Tower e DMATowerStream
+dmaTowerStream periph stream chan = do
+  sig <- signalUnsafe (Interrupt int)
+                    (Microseconds 10)
+                    (interrupt_disable int)
 
-  init_chan <- channel
-  dma_stream_0 <- mkTowerStream 0 (snd init_chan)
-  dma_stream_1 <- mkTowerStream 1 (snd init_chan)
-  dma_stream_2 <- mkTowerStream 2 (snd init_chan)
-  dma_stream_3 <- mkTowerStream 3 (snd init_chan)
-  dma_stream_4 <- mkTowerStream 4 (snd init_chan)
-  dma_stream_5 <- mkTowerStream 5 (snd init_chan)
-  dma_stream_6 <- mkTowerStream 6 (snd init_chan)
-  dma_stream_7 <- mkTowerStream 7 (snd init_chan)
-
-  monitor "dmaPeriph" $ do
-    handler systemInit "dmaPeriphInit" $ do
-      init_e <- emitter (fst init_chan) 1
-      callback $ \t -> do
-        dmaRCCEnable periph
-        emit init_e t
-
-  let dma_stream_periph = dmaName periph
-  return DMATowerStreams{..}
-
+  return (mkStream sig)
   where
-
-  mkTowerStream :: DMAStream
-                -> ChanOutput (Stored ITime)
-                -> Tower e DMATowerStream
-  mkTowerStream s init_chan = do
-    c <- signalUnsafe (Interrupt int)
-                      (Microseconds 10)
-                      (interrupt_disable int)
-
-    return (stream c)
-    where
-    int = streamInterrupt periph s
-    stream c = DMATowerStream
-      { dma_stream_init           = init_chan
-      , dma_stream_signal         = c
-      , dma_stream_regs           = getStreamRegs periph s
-      , dma_stream_enable_int     = interrupt_enable int
-      , dma_stream_get_isrflags   = getISRFlags   periph s
-      , dma_stream_clear_isrflags = clearISRFlags periph s
-      }
+  int = streamInterrupt periph stream
+  mkStream sig = DMATowerStream
+    { dma_stream_signal         = sig
+    , dma_stream_channel        = chan
+    , dma_stream_regs           = getStreamRegs periph stream
+    , dma_stream_enable_int     = interrupt_enable int
+    , dma_stream_get_isrflags   = getISRFlags   periph stream
+    , dma_stream_clear_isrflags = clearISRFlags periph stream
+    }
 
 
--- ivory-bsp-stm32/src/Ivory/BSP/STM32/Peripheral/UART/DMA.hs
+++ ivory-bsp-stm32/src/Ivory/BSP/STM32/Peripheral/UART/DMA.hs
@@ -3,13 +3,12 @@ module Ivory.BSP.STM32.Peripheral.UART.DMA where
 
 import Ivory.BSP.STM32.Peripheral.UART.Peripheral
 import Ivory.BSP.STM32.Peripheral.DMA
-import Ivory.BSP.STM32.Driver.DMA
 
 data DMAUART = DMAUART
   { dmaUARTPeriph    :: UART
   , dmaUARTDMAPeriph :: DMA
-  , dmaUARTTxStream  :: DMATowerStreams -> DMATowerStream
-  , dmaUARTTxChannel :: Int -- 0..8
-  , dmaUARTRxStream  :: DMATowerStreams -> DMATowerStream
-  , dmaUARTRxChannel :: Int -- 0..8
+  , dmaUARTTxStream  :: DMAStream -- int 0..7
+  , dmaUARTTxChannel :: DMAChannel -- int 0..7
+  , dmaUARTRxStream  :: DMAStream -- int 0..7
+  , dmaUARTRxChannel :: DMAChannel -- int 0..7
   }
