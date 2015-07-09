32b813d0a3ccff1566e129ea2443c229d7680213
Pat Hickey <pat@galois.com>
2015-07-08 22:58:48 -0700

    ivory-bsp-tests: change uart test echoprompt to old unbuffered version
    makes better demo

---
+++ ivory-bsp-tests/BSP/Tests/UART/Buffer.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module BSP.Tests.UART.Buffer where
+
+import Ivory.Language
+import Ivory.Stdlib
+import Ivory.Tower
+import Ivory.Tower.HAL.Bus.Interface
+import BSP.Tests.UART.Types
+
+uartTestTypes :: Module
+uartTestTypes = package "uartTestTypes" $ do
+  defStringType (Proxy :: Proxy UARTBuffer)
+
+uartUnbuffer :: BackpressureTransmit UARTBuffer (Stored IBool)
+             -> Tower e (ChanInput (Stored Uint8))
+uartUnbuffer (BackpressureTransmit req res) = do
+  c <- channel
+  p <- period (Milliseconds 10)
+  monitor "uart_unbuffer" $ do
+    flush_defer <- state "flush_defer"
+    buf <- state "buffer"
+    let ready_buf :: Ivory eff IBool
+        ready_buf = fmap (>? 0) (deref (buf ~> stringLengthL))
+
+        send_buf :: Emitter UARTBuffer -> Ivory eff ()
+        send_buf e = do
+          emit e (constRef buf)
+          store (buf ~> stringLengthL) 0
+          store flush_defer true
+
+    handler (snd c) "uart_byte_tosend" $ do
+      callbackV $ \byte -> do
+        pos <- deref (buf ~> stringLengthL)
+        when (pos <? arrayLen (buf ~> stringDataL)) $ do
+          store (buf ~> stringDataL ! toIx pos) byte
+          store (buf ~> stringLengthL) (pos + 1)
+
+    handler p "uart_tx_flush" $ do
+      e <- emitter req 1
+      callback $ const $ do
+        defer <- deref flush_defer
+        ready <- ready_buf
+        when (ready .&& iNot defer) (send_buf e)
+
+    handler res "uart_tx_res" $ do
+      callback $ const $ store flush_defer false
+
+  return (fst c)
+
+  where
+
--- ivory-bsp-tests/BSP/Tests/UART/TestApp.hs
+++ ivory-bsp-tests/BSP/Tests/UART/TestApp.hs
@@ -11,20 +11,16 @@ import Data.Char (ord)
 import Ivory.Language
 import Ivory.Stdlib
 import Ivory.Tower
-import Ivory.Tower.HAL.Bus.Interface
 
 import BSP.Tests.Platforms
 import BSP.Tests.LED.Blink
-import BSP.Tests.UART.Types
+import BSP.Tests.UART.Buffer
 
 import Ivory.BSP.STM32.Driver.UART
 import Ivory.BSP.STM32.ClockConfig
 
 --------------------------------------------------------------------------------
 
-uartTestTypes :: Module
-uartTestTypes = package "uartTestTypes" $ do
-  defStringType (Proxy :: Proxy UARTBuffer)
 
 app :: (e -> ColoredLEDs)
     -> (e -> ClockConfig)
@@ -35,81 +31,63 @@ app toleds tocc touart = do
   towerModule  uartTestTypes
 
   e <- getEnv
-  -- Starts two tasks: a blink task and a controller task.  Periodically blink
-  -- the blue LED.
-  blink p [blueLED (toleds e)]
-  -- A new queue
-  redledctl <- channel
-  -- Starts a UART (serial) task
+  -- A new message channel
+  (ledctl_input, ledctl_output) <- channel
+
+  -- Starts a UART (serial) driver
   let u = touart e
-  (BackpressureTransmit req res, ostream) <- uartTower tocc (testUARTPeriph u) (testUARTPins u)
+  (buffered_ostream, istream) <- uartTower tocc (testUARTPeriph u) (testUARTPins u)
                                                        115200
-  -- Start the task defined below
-  echoPrompt "hello world" req res ostream (fst redledctl)
-  -- A task that takes control input (Boolean) from the echo prompt and controls
-  -- the red LED based on it.
-  monitor "settableLED" $ ledController [redLED (toleds e)] (snd redledctl)
-  where
-  p = Milliseconds 333
+  -- UART buffer transmits in buffers. We want to transmit byte-by-byte and let
+  -- this monitor manage periodically flushing a buffer.
+  ostream <- uartUnbuffer buffered_ostream
+
+  -- Start the user interaction monitor defined below
+  echoPrompt "hello world" ostream istream ledctl_input
+
+  -- A monitor that takes control output (Boolean) from the echo prompt and
+  -- controls the blue LED
+  monitor "settableLED" $ ledController [redLED (toleds e)] ledctl_output
 
 --------------------------------------------------------------------------------
-
 echoPrompt :: String
-           -> ChanInput  UARTBuffer
-           -> ChanOutput (Stored IBool)
+           -> ChanInput  (Stored Uint8)
            -> ChanOutput (Stored Uint8)
            -> ChanInput  (Stored IBool)
            -> Tower p ()
-echoPrompt greeting req res ostream ledctl = do
+echoPrompt greeting ostream istream ledctl = do
   p <- period (Milliseconds 1)
 
+  let puts :: (GetAlloc eff ~ Scope cs)
+           => Emitter (Stored Uint8) -> String -> Ivory eff ()
+      puts e str = mapM_ (\c -> putc e (fromIntegral (ord c))) str
+
+      putc :: (GetAlloc eff ~ Scope cs)
+           => Emitter (Stored Uint8) -> Uint8 -> Ivory eff ()
+      putc = emitV
+
   monitor "echoprompt" $ do
-    (out_req :: Ref Global UARTBuffer) <- state "out_req"
-
-    let puts :: String -> Ivory eff ()
-        puts str = mapM_ (\c -> putc (fromIntegral (ord c))) str
-
-        putc :: Uint8 -> Ivory eff ()
-        putc byte = do
-          pos <- deref (out_req ~> stringLengthL)
-          when (pos <? arrayLen (out_req ~> stringDataL)) $ do
-            store (out_req ~> stringDataL ! toIx pos) byte
-            store (out_req ~> stringLengthL) (pos + 1)
-
-        flush :: (GetAlloc eff ~ Scope cs)
-              => Emitter UARTBuffer -> Ivory eff ()
-        flush e = do
-          emit e (constRef out_req)
-          store (out_req ~> stringLengthL) 0
-
     initialized <- stateInit "initialized" (ival false)
-
     handler p "init" $ do
-      e <- emitter req 1
+      o <- emitter ostream 32
       callback $ const $ do
         i <- deref initialized
         unless i $ do
           store initialized true
-          puts (greeting ++ "\r\n")
-          puts prompt
-          flush e
+          puts o (greeting ++ "\n")
+          puts o prompt
 
-    handler ostream "ostream" $ do
-      e <- emitter req 1
+    handler istream "istream" $ do
       l <- emitter ledctl 1
+      o <- emitter ostream 32
       callbackV $ \input -> do
-        -- Echo to terminal, replace newline with CR/LF for portability.
-        ifte_ (isChar input '\n' .|| isChar input '\r')
-          (puts $ "\r\n" ++ prompt)
-          (putc input)
+        putc o input -- echo to terminal
         let testChar = (input `isChar`)
         cond_
           [ testChar '1'  ==> emitV l true
           , testChar '2'  ==> emitV l false
+          , testChar '\n' ==> puts o prompt
           ]
-        flush e
-
-    handler res "result" $ return () -- XXX
   where prompt = "tower> "
 
 isChar :: Uint8 -> Char -> IBool
