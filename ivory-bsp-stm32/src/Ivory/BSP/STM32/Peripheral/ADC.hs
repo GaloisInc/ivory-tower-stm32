0ee73ae9fcef39fa0a3150141930dcb22fb5ba0e
Pat Hickey <pat@galois.com>
2015-08-31 16:48:08 -0700

    ivory-bsp-stm32: add Peripheral.ADC umbrella module

---
+++ ivory-bsp-stm32/src/Ivory/BSP/STM32/Peripheral/ADC.hs
@@ -0,0 +1,7 @@
+module Ivory.BSP.STM32.Peripheral.ADC
+  ( module Ivory.BSP.STM32.Peripheral.ADC.Peripheral
+  , module Ivory.BSP.STM32.Peripheral.ADC.RegTypes
+  ) where
+
+import Ivory.BSP.STM32.Peripheral.ADC.Peripheral
+import Ivory.BSP.STM32.Peripheral.ADC.RegTypes
