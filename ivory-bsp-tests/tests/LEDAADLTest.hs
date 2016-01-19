4975831b52a0b0732a5e3ef53d9d37857ad8a880
Jason Dagit <dagitj@gmail.com>
2016-01-18 16:02:49 -0800

    ivory-bsp-tests: Refactor tests to work with changes to tower-aadl

--- ivory-bsp-tests/tests/LEDAADLTest.hs
+++ ivory-bsp-tests/tests/LEDAADLTest.hs
@@ -6,22 +6,27 @@ import Ivory.Tower.Options
 import Tower.AADL
 
 import Ivory.Tower
+import Ivory.Tower.Config
 import Ivory.HW.Module
+
+import Tower.AADL.Build.Common
+import Tower.AADL.Build.EChronos
+
+import Ivory.BSP.STM32.Config
 
 import BSP.Tests.Platforms
 import BSP.Tests.LED.TestApp (app)
 
 main :: IO ()
-main = compileTowerAADL f p $ do
+main = compileTowerAADLForPlatform f p $ do
   app testplatform_leds
   mapM_ towerArtifact hw_artifacts
   where
-  f :: TestPlatform -> AADLConfig
-  f _ = defaultAADLConfig { configSystemOS = EChronos, configSystemHW = PIXHAWK }
+  f :: TestPlatform -> (AADLConfig, OSSpecific STM32Config)
+  f tp = ( defaultAADLConfig { configSystemOS = EChronos
+                             , configSystemHW = PIXHAWK
+                             }
+         , defaultEChronosOS (testplatform_stm32 tp)
+         )
   p :: TOpts -> IO TestPlatform
-  -- TODO JED: This test should really use the testPlatformParser, but first we
-  -- need to fix a bug in the interaction between the testPlatformParser and
-  -- the option parser used inside compileTowerAADL. For now the compromise
-  -- is to simply hardcode the platform.
-  p _ = return px4fmuv24
-  -- p topts = getConfig topts testPlatformParser
+  p topts = fmap fst (getConfig' topts testPlatformParser)
