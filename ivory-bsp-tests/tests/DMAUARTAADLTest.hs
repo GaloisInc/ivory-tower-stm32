9e60b5c497bc77c4ee4bf3af716303d0b0bae2b7
Jason Dagit <dagit@galois.com>
2016-06-02 18:05:38 -0700

    ivory-bsp-tests: add uart dma test

---
+++ ivory-bsp-tests/tests/DMAUARTAADLTest.hs
@@ -0,0 +1,30 @@
+
+module Main where
+
+import Ivory.Tower.Config
+import Ivory.Tower.Options
+
+import Ivory.OS.FreeRTOS.Tower.STM32
+
+import Tower.AADL
+
+import Tower.AADL.Build.Common
+import Tower.AADL.Build.EChronos
+
+import BSP.Tests.Platforms
+import BSP.Tests.DMAUART.TestApp (app)
+
+main :: IO ()
+main = compileTowerAADLForPlatform f p $ do
+        app testplatform_leds
+            (stm32config_clock . testplatform_stm32)
+            testplatform_dma
+  where
+  f :: TestPlatform -> (AADLConfig, OSSpecific STM32Config)
+  f tp = ( defaultAADLConfig { configSystemOS = EChronos
+                             , configSystemHW = PIXHAWK
+                             }
+         , defaultEChronosOS (testplatform_stm32 tp)
+         )
+  p :: TOpts -> IO TestPlatform
+  p topts = fmap fst (getConfig' topts testPlatformParser)
