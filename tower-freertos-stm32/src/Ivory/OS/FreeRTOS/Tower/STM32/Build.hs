68bf7eaf36af7903c2232d079946ff1ce22f5bf4
Adam C. Foltzer <acfoltzer@galois.com>
2017-01-23 16:53:42 -0800

    Revert "Fixed upload script so the UPLOAD_PORT variable is no longer needed"
    This reverts commit 89651cd11b14f2dd89e70b1759e9aeb4fc750396.

--- tower-freertos-stm32/src/Ivory/OS/FreeRTOS/Tower/STM32/Build.hs
+++ tower-freertos-stm32/src/Ivory/OS/FreeRTOS/Tower/STM32/Build.hs
@@ -81,7 +81,8 @@ makefile STM32Config{..} userobjs = Root $ artifactString "Makefile" $ unlines
       , "\t$(CC) -o $@ $(LDFLAGS) -Wl,--script=bl_linker_script.lds -Wl,-Map=$@.map $(OBJS) $(LDLIBS)"
       , ""
       , "upload: image.px4"
-      , "\tpython px_uploader.py --port=/dev/serial/by-id/usb-3D_Robotics* $<"
+      , "\t@echo \"*** User expected to set UPLOAD_PORT environment variable ***\""
+      , "\tpython px_uploader.py --port=$(UPLOAD_PORT) $<"
       , ""
       ]
 
