4800099dcf361ccf786a1fffcb863e2ea9dd2f56
Richard Marko <srk@48.io>
2017-03-20 20:54:22 +0100

    ivory-bsp-stm32: add ADC to F405

---
+++ ivory-bsp-stm32/src/Ivory/BSP/STM32F405/ADC.hs
@@ -0,0 +1,43 @@
+module Ivory.BSP.STM32F405.ADC
+  ( adc1
+  , adc2
+  , adc3
+  ) where
+
+import Ivory.Language
+import Ivory.HW
+
+import Ivory.BSP.STM32.Peripheral.ADC
+import Ivory.BSP.STM32F405.Interrupt
+import Ivory.BSP.STM32F405.MemoryMap
+import Ivory.BSP.STM32F405.RCC
+
+adc1 :: ADCPeriph
+adc1 = mkADCPeriph adc1_periph_base
+        adc1_periph_base
+        (rccEnable rcc_apb2en_adc1)
+        (rccDisable rcc_apb2en_adc1)
+        ADC
+        "adc1"
+
+adc2 :: ADCPeriph
+adc2 = mkADCPeriph adc2_periph_base
+        adc1_periph_base
+        (rccEnable rcc_apb2en_adc2)
+        (rccDisable rcc_apb2en_adc2)
+        ADC
+        "adc2"
+
+adc3 :: ADCPeriph
+adc3 = mkADCPeriph adc3_periph_base
+        adc1_periph_base
+        (rccEnable rcc_apb2en_adc3)
+        (rccDisable rcc_apb2en_adc3)
+        ADC
+        "adc3"
+
+rccEnable :: BitDataField RCC_APB2ENR Bit -> Ivory eff ()
+rccEnable field = modifyReg regRCC_APB2ENR $ setBit field
+
+rccDisable :: BitDataField RCC_APB2ENR Bit -> Ivory eff ()
+rccDisable field = modifyReg regRCC_APB2ENR $ clearBit field
